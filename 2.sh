e2d205db5609a7c6c6a583253c0be56a828799ff
OverLord-Git <69304409+OverLord-Git@users.noreply.github.com>
2025-02-14 13:11:20 -0400

    Update 2.sh

--- 2.sh
+++ 2.sh
@@ -1,36 +1,35 @@
 #!/bin/bash
 # --------------------------------------------
 # HybridOS Android & Tools Installer
-# Autor: Chief Architect OS
 # --------------------------------------------
 
-# ===== INSTALACIÓN DE HERRAMIENTAS =====
-install_tools() {
+# Configuración
+LOG_FILE="/var/log/hybridos_android.log"
+export LC_ALL=en_US.UTF-8
+
+log() {
+  echo "[$(date '+%Y-%m-%d %H:%M:%S')] $1" | tee -a $LOG_FILE
+}
+
+install_stack() {
   log "Instalando herramientas esenciales..."
   sudo pacman -S --noconfirm \
-    yay flatpak unzip unrar p7zip \
-    wine-staging winetricks protontricks \
-    steam docker
+    yay flatpak wine-staging winetricks \
+    steam proton-ge-custom-bin docker \
+    lxc lxd waydroid
 
+  log "Configurando Docker..."
   sudo systemctl enable docker
   sudo usermod -aG docker $USER
 
-  flatpak install -y \
-    com.discordapp.Discord \
-    com.spotify.Client \
-    org.blender.Blender
-}
-
-# ===== CONFIGURAR ENTORNO DE GAMING =====
-setup_gaming() {
-  log "Configurando Steam y Proton..."
-  yay -S --noconfirm proton-ge-custom-bin
-  mkdir -p ~/.steam/root/compatibilitytools.d
-  cp -r /usr/share/steam/compatibilitytools.d/* ~/.steam/root/compatibilitytools.d/
+  log "Iniciando Waydroid..."
+  sudo systemctl start lxd
+  waydroid init
 }
 
 main() {
-  install_tools
-  setup_gaming
-  log "Herramientas y gaming configurados!"
+  install_stack
+  log "Capa Android instalada! Ejecute ai_subsystem.sh"
 }
+
+main "$@"
